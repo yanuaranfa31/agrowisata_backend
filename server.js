ea26ea2188c2a8ce8c0efb5cbc01608dbe95a112
Macintosh HD <macintoshhd@Macintoshs-MacBook-Pro.local>
2024-12-02 22:00:57 +0700

    push

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Impor cors
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
 const bookingRoutes = require('./routes/booking');
 const bookingRoutesuser = require('./routes/bookinguser');
-const  protect  = require('./middleware/authMiddleware');
-const kuotaRouts = require('./routes/kuota')
+const protect = require('./middleware/authMiddleware');
+const kuotaRouts = require('./routes/kuota');
+
 // Mengambil variabel dari .env
 dotenv.config();
 
 // Inisialisasi express
 const app = express();
+
+// Gunakan middleware CORS
+app.use(cors()); // Menambahkan CORS sebagai middleware
 
 // Middleware untuk body parser
 app.use(bodyParser.json());
@@ -23,8 +28,8 @@ connectDB();
 app.use('/api/auth', authRoutes);
 app.use('/api/booking', protect, bookingRoutesuser);
 app.use('/api/bookinguser', bookingRoutes);
+app.use('/api/kuota', kuotaRouts);
 
-app.use('/api/kuota',kuotaRouts);
 // Menjalankan server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
