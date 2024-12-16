d40e2bd562b9eea8c82a3c6a0644afb188714ac1
Yanuar <22sa11a014@mhs.amikompurwokerto.ac.id>
2024-12-16 13:04:33 +0700

    menambahkan jolom email

--- models/Booking.js
+++ models/Booking.js
@@ -10,6 +10,10 @@ const BookingSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+  },
   numberOfTickets: {
     type: Number,
     required: true,
